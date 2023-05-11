11166d23c192eaa43194b86c4b9e0dd4f66b2b28
syelii12 <117338933+syelii12@users.noreply.github.com>
2023-05-11 14:48:08 +0700

    Add files via upload

---
+++ harberweb.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#btnsearch").click(function(e){
+        var txtInput = $("#srcInv").val();
+        console.log(txtInput);
+        $("#infoinvaccr").html("Nama Anda : " + txtInput);
+        $.ajax({
+            type: "POST",
+            url: "/assets/scripts/ajax/getcurl.php",
+            data: "",
+            success: function (response) {
+                $("#infoinvaccr").html("<br/>Response<br/>" + response);
+            }
+        });
+    });
+});
